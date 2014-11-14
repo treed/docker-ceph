d97ed9119516a9541f1d3bc8b5b75d264bce6706
Ted Reed <ted.reed@gmail.com>
2014-11-14 01:43:21 -0800

    error handling

--- ceph-ebs-mount/entrypoint.sh
+++ ceph-ebs-mount/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
+function die() {
+    echo $1
+    exit 1
+}
+
 : ${CLUSTER_NAME:=ceph}
+: ${DEVICE:=sdf}
 
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-VOLUME_ID=$(curl http://172.17.42.1:4001/v2/keys/ceph/$CLUSTER_NAME/osd/$OSD_ID/ebs-volume)
+INSTANCE_ID=$(curl -q http://169.254.169.254/latest/meta-data/instance-id)
+VOLUME_ID=$(curl -q http://172.17.42.1:4001/v2/keys/ceph/$CLUSTER_NAME/osd/$OSD_ID/ebs-volume)
 
-aws ec2 attach-volume --instance-id $INSTANCE_ID --volume-id $VOLUME_ID --device /dev/sdf
+test -n "$VOLUME_ID" || die "Unable to find VOLUME_ID for OSD $OSD_ID"
+
+aws ec2 attach-volume --instance-id $INSTANCE_ID --volume-id $VOLUME_ID --device /dev/$DEVICE
 mkdir -pf /var/lib/ceph/${CLUSTER_NAME}-${OSD_ID}
-mount /dev/sdf /var/lib/ceph/${CLUSTER_NAME}-${OSD_ID}
+mount /dev/$DEVICE /var/lib/ceph/${CLUSTER_NAME}-${OSD_ID}
--- ceph-ebs-unmount/entrypoint.sh
+++ ceph-ebs-unmount/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -e
 
+function die() {
+    echo $1
+    exit 1
+}
+
 : ${CLUSTER_NAME:=ceph}
+: ${DEVICE:=sdf}
 
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-VOLUME_ID=$(curl http://172.17.42.1:4001/v2/keys/ceph/$CLUSTER_NAME/osd/$OSD_ID/ebs-volume)
+INSTANCE_ID=$(curl -q http://169.254.169.254/latest/meta-data/instance-id)
+VOLUME_ID=$(curl -q http://172.17.42.1:4001/v2/keys/ceph/$CLUSTER_NAME/osd/$OSD_ID/ebs-volume)
 
-umount /dev/sdf /var/lib/ceph/${CLUSTER_NAME}-${OSD_ID}
-aws ec2 detach-volume --instance-id $INSTANCE_ID --volume-id $VOLUME_ID --device /dev/sdf
+test -n "$VOLUME_ID" || die "Unable to find VOLUME_ID for OSD $OSD_ID"
+
+umount /dev/$DEVICE /var/lib/ceph/${CLUSTER_NAME}-${OSD_ID}
+aws ec2 detach-volume --instance-id $INSTANCE_ID --volume-id $VOLUME_ID --device $DEVICE
