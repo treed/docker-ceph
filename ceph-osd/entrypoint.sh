24a5c669340bc609b67f5869812099942eabb869
Ted Reed <ted.reed@gmail.com>
2015-02-07 20:05:21 -0800

    etcdify osd

--- ceph-osd/entrypoint.sh
+++ ceph-osd/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-set -e
+set -ex
 
-: ${CLUSTER:=ceph}
-: ${WEIGHT:=1.0}
-: ${JOURNAL:=/var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/journal}
+ETCDCTL_PEERS=172.17.42.1:4001
 
-# Make sure the osd id is set
 if [ ! -n "$OSD_ID" ]; then
    echo "OSD_ID must be set; call 'ceph osd create' to allocate the next available osd id"
    exit 1
 fi
+
+: ${CLUSTER_NAME:=ceph}
+: ${WEIGHT:=1.0}
+: ${JOURNAL:=/var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/journal}
+: ${DATA:=/var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/data}
+
+fsid=$(etcdctl get /ceph/$CLUSTER_NAME/fsid)
 
 # Make sure osd directory exists
 mkdir -p /var/lib/ceph/osd/${CLUSTER}-${OSD_ID}
@@ -17,13 +21,11 @@ mkdir -p /var/lib/ceph/osd/${CLUSTER}-${OSD_ID}
 # Check to see if our OSD has been initialized
 if [ ! -e /var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/keyring ]; then
    # Create OSD key and file structure
-   ceph-osd -i $OSD_ID --mkfs --mkjournal --osd-journal ${JOURNAL}
+   ceph-osd -i $OSD_ID --mkfs --osd-data ${DATA} --mkjournal --osd-journal ${JOURNAL}
 
-   # Add OSD key to the authentication database
-   if [ ! -e /etc/ceph/${CLUSTER}.client.admin.keyring ]; then
-      echo "Cannot authenticate to Ceph monitor without /etc/ceph/${CLUSTER}.client.admin.keyring.  Retrieve this from /etc/ceph on a monitor node."
-      exit 1
-   fi
+   echo Getting client key from etcd
+   etcdctl get /ceph/$CLUSTER_NAME/keyrings/client-admin > /etc/ceph/${CLUSTER_NAME}.client.admin.keyring
+
    ceph auth get-or-create osd.${OSD_ID} osd 'allow *' mon 'allow profile osd' -o /var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/keyring
 
    # Add the OSD to the CRUSH map
